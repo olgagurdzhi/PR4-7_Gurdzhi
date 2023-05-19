731c088df653d94d3db5e2f0023b5ee3720ac988
Andrey <mr.dallas007@gmail.com>
2023-05-19 17:45:03 +0300

    Add 2 transaction buttons

--- app/src/main/java/com/example/pr4_gurdzhi/BlankFragment2.java
+++ app/src/main/java/com/example/pr4_gurdzhi/BlankFragment2.java
@@ -1,64 +1,61 @@
 package com.example.pr4_gurdzhi;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BlankFragment2#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Random;
+
 public class BlankFragment2 extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
+    private Context context;
+    private final int duration = Toast.LENGTH_SHORT;
+    private static final String TAG = "BlankFragment2";
     public BlankFragment2() {
-        // Required empty public constructor
+        super(R.layout.fragment_blank2);
     }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Random r = new Random();
+        int randomNumber = (int)(Math.random()*100000);
+        TextView ticket = view.findViewById(R.id.ticket);
+        ticket.setText(String.valueOf(randomNumber));
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment2.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BlankFragment2 newInstance(String param1, String param2) {
-        BlankFragment2 fragment = new BlankFragment2();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+        Button button = view.findViewById(R.id.buttonToList1);
+        button.setOnClickListener(v -> {
+            Bundle result = new Bundle();
+            result.putString("bundleKey", String.valueOf(randomNumber));
+            getParentFragmentManager().setFragmentResult("requestKey", result);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+            getParentFragmentManager().beginTransaction().replace(R.id.fragment_container_view,
+                            BlankFragment3.class, null)
+                    .commit();
+        });
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank2, container, false);
+        Button button2 = view.findViewById(R.id.buttonToList2);
+        button.setOnClickListener(v -> {
+            Bundle result = new Bundle();
+            result.putString("bundleKey", String.valueOf(randomNumber));
+            getParentFragmentManager().setFragmentResult("requestKey", result);
+            getParentFragmentManager().beginTransaction().replace(R.id.fragment_container_view,
+                            BlankFragment4.class, null)
+                    .commit();
+        });
+
+
+
+
     }
 }
